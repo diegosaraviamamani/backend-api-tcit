c7cf7c69791160fd2f6667e90caf602efcc1dd10
Diego Saravia Mamani <63614444+diegosaraviamamani@users.noreply.github.com>
2022-02-26 03:56:46 -0400

    fix: cors allowed

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+  credentials: true
+};
 
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts')
@@ -15,6 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/posts', postsRouter);
+app.use('/posts', cors(corsOptions), postsRouter);
 
 module.exports = app;
